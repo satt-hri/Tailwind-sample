a684b6c0103a38430eed9515e17090359c5e28e4
h_ri <h_ri@satt.jp>
2023-08-10 14:58:29 +0900

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  alias:{
+    '@':resolve(__dirname,"/")
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
