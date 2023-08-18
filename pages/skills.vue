5b30db227e2821e1a80ec24b030e181719954d12
h_ri <h_ri@satt.jp>
2023-08-18 09:52:30 +0900

    画面追加

---
+++ pages/skills.vue
@@ -0,0 +1,46 @@
+<template>
+    <div name="skills" class="w-full h-screen bg-[#0a192f]  text-gray-300">
+        <div class="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+            <div>
+                <p class='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+                <p class='py-4'>// These are the technologies I've worked with</p>
+            </div>
+            <div class="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center">
+                <div v-for="item in list" class="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                    <img class="w-20 mx-auto" :src="`/images/${item.name}`" :alt="item.name" />
+                    <p class="my-4">{{ item.text }}</p>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </div>
+</template>
+
+<script setup lang="ts">
+type Item = { text: string, name: string }
+const generateImgPath = (fileName: string): string => {
+    return new URL(`@/images/${fileName}`, import.meta.url).href
+}
+
+const list: Array<Item> = [
+    { text: "HTML", name: "html.png" },
+    { text: "CSS", name: "css.png" },
+    { text: "JavaScript", name: "javascript.png" },
+    { text: "jQuery", name: "jquery.png" },
+    { text: "Vue", name: "vue.png" },
+    { text: "Nuxt", name: "nuxt.svg" },
+    { text: "React", name: "react.png" },
+    { text: "NextJS", name: "nextjs.svg" },
+    { text: "GitHub", name: "css.png" },
+    { text: "Node", name: "css.png" },
+    { text: "Mongo", name: "mongo.png" },
+    { text: "TAILWIND", name: "tailwind.png" },
+    { text: "FIREBASE", name: "firebase.png" },
+]
+
+</script>
+
+<style scoped></style>
